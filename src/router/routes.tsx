90c7066e2ec464cd6a486dafab88a03adfba2927
Farkhadov95 <farkhadov950405@gmail.com>
2024-02-13 12:33:01 +0500

    Added Error page

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Registration from "../Pages/Registration";
 import AdminPanel from "../Pages/AdminPanel";
 import Login from "../Pages/Login";
+import ErrorPage from "../Pages/NotFound";
 
 const router = createBrowserRouter([
   { path: "/", element: <Login /> },
   { path: "/signup", element: <Registration /> },
   { path: "/admin", element: <AdminPanel /> },
+  { path: "*", element: <ErrorPage /> },
 ]);
 
 export default router;
